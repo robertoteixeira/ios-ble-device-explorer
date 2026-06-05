28cdb3369ee13cbd2bf61b940cf1fe92b38165ea
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-06-05 16:34:06 -0300

    Initial Xcode project setup

---
+++ CoreBluetoothExplorer/CoreBluetoothExplorer/CoreBluetoothExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothExplorerApp.swift
+//  CoreBluetoothExplorer
+//
+//  Created by Roberto Teixeira on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
