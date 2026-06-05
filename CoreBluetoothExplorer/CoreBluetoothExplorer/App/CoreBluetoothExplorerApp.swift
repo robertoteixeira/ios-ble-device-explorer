000986e9e8cc8b8cede34d5498a1a089915adf75
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-06-05 17:50:33 -0300

    Organize app folders and add Bluetooth permission

--- CoreBluetoothExplorer/CoreBluetoothExplorer/App/CoreBluetoothExplorerApp.swift
+++ CoreBluetoothExplorer/CoreBluetoothExplorer/App/CoreBluetoothExplorerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CoreBluetoothExplorerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DeviceScannerView()
         }
     }
 }
